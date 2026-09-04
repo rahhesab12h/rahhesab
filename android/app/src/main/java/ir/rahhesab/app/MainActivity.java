e211288da4883033fa2101d06a92c2e35a31a439
RahHesab <rahhesab@app.local>
2026-09-04 19:10:12 +0330

    Add Bazaar billing bridge

--- android/app/src/main/java/ir/rahhesab/app/MainActivity.java
+++ android/app/src/main/java/ir/rahhesab/app/MainActivity.java
@@ -1,5 +1,19 @@
 package ir.rahhesab.app;
 
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        WebView webView = bridge.getWebView();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(
+            new BazaarBridge(this),
+            "BazaarBridge"
+        );
+    }
+}
