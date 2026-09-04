a12815cd1574139d6b8e186e1572549737d471c8
RahHesab <rahhesab@app.local>
2026-09-04 19:13:26 +0330

    Revert "Add Bazaar billing bridge"
    This reverts commit e211288da4883033fa2101d06a92c2e35a31a439.

--- android/app/src/main/java/ir/rahhesab/app/MainActivity.java
+++ android/app/src/main/java/ir/rahhesab/app/MainActivity.java
@@ -1,19 +1,5 @@
 package ir.rahhesab.app;
 
-import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        WebView webView = bridge.getWebView();
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.addJavascriptInterface(
-            new BazaarBridge(this),
-            "BazaarBridge"
-        );
-    }
-}
+public class MainActivity extends BridgeActivity {}
